93ae18acc6a60b5e841d92ea0db36cf8e93945e2
hitheima <hitheima@163.com>
2019-05-29 20:54:40 +0800

    登录 - 基本操作

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,16 +1,26 @@
 import time
 
 from base.base_driver import init_driver
+from page.page import Page
 
 
 class TestLogin:
 
     def setup(self):
         self.driver = init_driver()
+        self.page = Page(self.driver)
 
     def teardown(self):
-        time.sleep(3)
+        time.sleep(5)
         self.driver.quit()
 
     def test_login(self):
-        print("hello")
+        self.page.home.click_me()
+        self.page.register.click_login()
+        self.page.login.input_username("itheima_test")
+        self.page.login.input_password("itheima")
+        self.page.login.click_login()
+
+        assert self.page.me.get_nick_name_text() == "itheima_test", "登录后的用户名和输入的用户民不一致"
+
+
