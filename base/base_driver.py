73242a8a027165323786b3162c4407329ec461b5
hitheima <hitheima@163.com>
2019-05-30 00:03:27 +0800

    登录 - 封装 toast 方法&完善登录脚本

--- base/base_driver.py
+++ base/base_driver.py
@@ -8,6 +8,8 @@ def init_driver():
     desired_caps['deviceName'] = '192.168.56.101:5555'
     desired_caps['appPackage'] = 'com.yunmall.lc'
     desired_caps['appActivity'] = 'com.yunmall.ymctoc.ui.activity.MainActivity'
+    # toast
+    desired_caps['automationName'] = 'Uiautomator2'
 
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
     return driver
