2a2b6146e42ef5212992f4e8b3fb91011fa07181
hitheima <hitheima@163.com>
2019-05-30 23:44:47 +0800

    如果没有登录就登录

--- page/home_page.py
+++ page/home_page.py
@@ -11,3 +11,19 @@ class HomePage(BaseAction):
     # 点击 我
     def click_me(self):
         self.click(self.me_button)
+
+    def login_if_not(self, page):
+        # 判断登录状态
+        self.click_me()
+        if self.driver.current_activity != "com.yunmall.ymctoc.ui.activity.LogonActivity":
+            return
+
+        # 没有登录，就去登录
+        # 点击 已有账号
+        page.register.click_login()
+        # 输入 用户名
+        page.login.input_username("itheima_test")
+        # 输入 密码
+        page.login.input_password("itheima")
+        # 点击 登录
+        page.login.click_login()
