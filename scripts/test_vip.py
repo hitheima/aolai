d2f3a2a75e816a0648c20349d61243cd5a9ba664
hitheima <hitheima@163.com>
2019-06-01 22:39:26 +0800

    加入VIP - 轮询查找 page_source

--- scripts/test_vip.py
+++ scripts/test_vip.py
@@ -1,3 +1,5 @@
+import time
+
 from base.base_driver import init_driver
 from page.page import Page
 
@@ -19,5 +21,21 @@ class TestVip:
         self.page.vip.input_invite("hello")
         # vip 点击 加入会员
         self.page.vip.click_be_vip()
+
+        # 断言，"邀请码输入不正确" 是否在 page_source 中
+        assert self.is_can_not_be_vip()
+
         # 切换 原生环境
         self.driver.switch_to.context("NATIVE_APP")
+
+    def is_can_not_be_vip(self):
+        """
+        如果不能成为会员，有"邀请码输入不正确"字符串，返回True
+        :return:
+        """
+        while True:
+            if "邀请码输入不正确" in self.driver.page_source:
+                print("有")
+                return True
+            else:
+                print("没有")
