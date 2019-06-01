4c10ed6545553030f2f301d04d8f01d7f077deec
hitheima <hitheima@163.com>
2019-06-01 19:16:24 +0800

    版本更新

--- page/me_page.py
+++ page/me_page.py
@@ -8,5 +8,11 @@ class MePage(BaseAction):
     # 昵称
     nick_name_text_view = By.ID, "com.yunmall.lc:id/tv_user_nikename"
 
+    # 设置按钮
+    setting_button = By.ID, "com.yunmall.lc:id/ymtitlebar_left_btn_image"
+
     def get_nick_name_text(self):
         return self.find_element(self.nick_name_text_view).text
+
+    def click_setting(self):
+        self.click(self.setting_button)
