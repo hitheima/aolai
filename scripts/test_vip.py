c8c1119f999dc4a437fbeaf7789b5639aecb9e70
hitheima <hitheima@163.com>
2019-06-03 13:19:46 +0800

    加入VIP - 参数化

--- scripts/test_vip.py
+++ scripts/test_vip.py
@@ -1,5 +1,6 @@
-import time
+import pytest
 
+from base.base_analyze import analyze_file
 from base.base_driver import init_driver
 from page.page import Page
 
@@ -10,7 +11,12 @@ class TestVip:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    def test_vip(self):
+    @pytest.mark.parametrize("args", analyze_file("vip_data.yaml", "test_vip"))
+    def test_vip(self, args):
+
+        keyword = args["keyword"]
+        expect = args["expect"]
+
         # 如果没有登录 去登陆
         self.page.home.login_if_not(self.page)
         # 我 点击 加入vip
@@ -18,12 +24,12 @@ class TestVip:
         # 切换 web环境
         self.driver.switch_to.context("WEBVIEW_com.yunmall.lc")
         # vip 输入 邀请码
-        self.page.vip.input_invite("hello")
+        self.page.vip.input_invite(keyword)
         # vip 点击 加入会员
         self.page.vip.click_be_vip()
 
         # 断言，"邀请码输入不正确" 是否在 page_source 中
-        assert self.page.vip.is_keyword_in_page_source("邀请码输入不正确"), "邀请码输入不正确不在page_source中"
+        assert self.page.vip.is_keyword_in_page_source(expect), "%s不在page_source中" % expect
 
         # 切换 原生环境
         self.driver.switch_to.context("NATIVE_APP")
